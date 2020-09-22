671fc0e714aec4a8a780d67cb1c18295b3ab7fab
Rattan Pal Kaur <rattan.kaur@BRILLIO.COM>
2020-09-22 14:08:42 +0530

    First commit

---
+++ build/deploy-prod.sh
@@ -0,0 +1,9 @@
+#Convert to MDAPI format for deployment to prod
+#echo "Converting to MDAPI format..."
+#sfdx force:source:convert -d deploy_prod -r force-app 
+#Deploy to prod & run all tests
+#echo "Deploying to production & running all tests..."
+#sfdx force:mdapi:deploy -u DevHub -d ./mdAPIZip/unpackaged -w -1 -l RunAllTestsInOrg 
+echo "Deploying metadata api"
+echo "Deploying to production"
+sfdx force:mdapi:deploy -u DevHub -d ./mdAPIZip -w 100 
